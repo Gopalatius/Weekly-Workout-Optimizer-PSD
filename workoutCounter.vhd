5a6b3d48a50c74a042779ff4d101f76b6dd4c064
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-03 16:00:29 +0700

    finishing workoutCounter component

--- workoutCounter.vhd
+++ workoutCounter.vhd
@@ -6,32 +6,52 @@ USE ieee.std_logic_unsigned.ALL;
 ENTITY wrkCount IS
     	PORT 
     	(
-
+            
     	    	ALL_0, TOGGLE, OPTIMAL : IN STD_LOGIC;
     	    	BTN_7 : IN STD_LOGIC;
 
-    	    	Q_JML_WORKOUT : OUT STD_LOGIC_VECTOR(0 TO 3);
     	    	IS_7, OPT_Q2, OPT_Q2aks : OUT STD_LOGIC
+
     	);
 END wrkCount;
 
 ARCHITECTURE wrkCountArch OF wrkCount IS
     	SIGNAL jmlWorkClk : STD_LOGIC;
-    	SIGNAL count : STD_LOGIC_VECTOR(0 TO 3) := "0000";
+    	SIGNAL optWorkClk : STD_LOGIC;
+    	SIGNAL jmlWorkCount : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
+    	SIGNAL optWorkCount : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
 
 BEGIN
     	jmlWorkClk <= ALL_0 AND TOGGLE;
-    	PROCESS (BTN_7, jmlWorkClk)
+    	optWorkClk <= jmlWorkClk AND OPTIMAL;
+
+    	PROCESS (BTN_7, jmlWorkClk, optWorkClk)
     	BEGIN
     	    	IF (BTN_7 = '1') THEN
-    	    	    	count <= "0000";
+ 
+    	    	    	jmlWorkCount <= "0000";
 
     	    	ELSIF (rising_edge(jmlWorkClk)) THEN
-    	    	    	count <= count + 1;
+
+    	    	    	jmlWorkCount <= jmlWorkCount + 1;
+
+    	    	END IF;
+
+    	    	IF (BTN_7 = '1') THEN
+
+    	    	    	optWorkCount <= "0000";
+
+    	    	ELSIF (rising_edge(optWorkClk)) THEN
+
+    	    	    	optWorkCount <= optWorkCount + 1;
 
     	    	END IF;
 
     	END PROCESS;
-    	Q_JML_WORKOUT <= count;
-    	IS_7 <= Q_JML_WORKOUT(0) AND Q_JML_WORKOUT(1) AND Q_JML_WORKOUT(2);
+
+    	IS_7 <= jmlWorkCount(0) AND jmlWorkCount(1) AND jmlWorkCount(2);
+
+    	OPT_Q2 <= optWorkCount(2);
+    	OPT_Q2aks <= NOT(optWorkCount(2));
+
 END wrkCountArch;
