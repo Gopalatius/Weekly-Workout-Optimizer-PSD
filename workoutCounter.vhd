de5d33dbe85c85045ee7a1ea7ef243b0c8ab1f9d
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-03 14:46:10 +0700

    adding workoutCounter component

---
+++ workoutCounter.vhd
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY wrkCount IS
+    	PORT 
+    	(
+
+    	    ALL_0, TOGGLE, OPTIMAL : IN STD_LOGIC;
+    	    IS_7, OPT_Q2, OPT_Q2' : OUT STD_LOGIC
+
+ 
+    	);
+END wrkCount;
