3cb3ab5316a4469f20158ed292dafc85baffb96d
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-07 00:59:39 +0700

    Ingin mencoba atur state tapi sudah ngantuk

--- Main.vhd
+++ Main.vhd
@@ -9,26 +9,37 @@
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
 ENTITY fsm IS
 	PORT
 	(
-		I, CLK : IN  STD_LOGIC;
-		Soda   : OUT STD_LOGIC
+		BTN : IN STD_LOGIC;
+		A1, B1, C1, D1, E1, F1, G1,
+		A2, B2, C2, D2, E2, F2, G2,
+		A3, B3, C3, D3, E3, F3, G3,
+		A4, B4, C4, D4, E4, F4, G4, : OUT STD_LOGIC
 	);
 END fsm;
 
 ARCHITECTURE arc_fsm OF fsm IS
 	TYPE states IS (ST0, ST1, ST2, ST3, ST4, ST5, ST6, ST7);
-	SIGNAL PS, NS : states;
+	SIGNAL PS, NS                              : states;
+	SIGNAL ALL_0, TOGGLE, OPTIMAL, BTN_7, IS_7 : STD_LOGIC;
+	SIGNAL ALL_0_AND_TOGGLE                    : STD_LOGIC;
+	SIGNAL JUMLAH_WORKOUT                      : STD_LOGIC;
 BEGIN
-	sync_proc : PROCESS (CLK, NS)
+	ALL_0_AND_TOGGLE <= ALL_0 AND TOGGLE;
+
+	sync_proc : PROCESS (ALL_0_AND_TOGGLE) IS
 	BEGIN
-		IF (rising_edge(I)) THEN
+		IF (rising_edge(ALL_0_AND_TOGGLE)) THEN
 			PS <= NS;
 		END IF;
 	END PROCESS;
 
-	comb_proc : PROCESS (PS, I)
+	comb_proc : PROCESS (PS, I) IS
 	BEGIN
 		Soda <= '0';
 
