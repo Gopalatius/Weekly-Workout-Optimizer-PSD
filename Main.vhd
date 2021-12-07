3b2883c4d07e231012df6e26e8f3670a24ea8e5d
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-07 23:56:28 +0700

    Intermediate Signal is done.

--- Main.vhd
+++ Main.vhd
@@ -13,41 +13,59 @@ USE ieee.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
 
 ENTITY fsm IS
-
 END fsm;
 
 ARCHITECTURE arc_fsm OF fsm IS
 	--ini input dari user. Harusnya ada button, tetapi
 	-- karena simulasi jadi dianggap sinyal saja
 	signal BTN : STD_LOGIC;
-	-- BTN -> Toggle
+	-- BTN -> Toggle, Countdown Counter
 	
 	
 	--ini dari Analog to Digital Converter.
 	--Thermistor mendeteksi workout optimal.
 	--jangan tertukar dengan OPT_Q2 nanti
 	signal OPTIMAL: STD_LOGIC
-	-- OPTIMAL OptimalLogic* -> OptimalNotification, FSM
+	-- OPTIMAL : OptimalLogic* -> OptimalNotification, FSM
 	
 	
-	--ini buffer dari NICE & POOR logic ke 7segment asli.
 	signal real_O1, real_O2, real_O3, real_O4: STD_LOGIC (6 downto 0);
+	--Nice&PoorLogic -> Real7Segment
 
 
-	SIGNAL ALL_0, TOGGLE, BTN_7, IS_7 : STD_LOGIC;
-	-- ALL_0  dari OtherLogic ke fsm ini
-	-- TOGGLE dari Toggle ke Clock, Other Logic, dan fsm ini
+	SIGNAL ALL_0, TOGGLE, BTN_7, IS_7, TGL_7 : STD_LOGIC;
+	-- ALL_0  : OtherLogic -> FSM
+	-- TOGGLE : Toggle -> Clock, OtherLogic
+	-- BTN_7 : OtherLogic -> FSM
+	-- IS_7 : FSM -> OtherLogic, Nice&PoorLogic
+	-- TGL_7 : OtherLogic -> Countdown Counter
 	
-	--komponen FSM
+	
+	SIGNAL Q : STD_LOGIC_VECTOR (7 downto 0);
+	SIGNAL D1, D2 : STD_LOGIC_VECTOR (6 downto 0);
+	-- Q : Countdown Counter -> OtherLogic
+	-- D1, D2 : Countdown Counter -> OtherLogic
+	
+	
+	SIGNAL CLK, CLK_STOP : STD_LOGIC;
+	-- CLK : Clock -> OtherLogic
+	-- CLK_STOP : OtherLogic -> Countdown Counter
+	
+	
+	--komponen FSM.
+	-- states berhubungan dengan JUMLAH_WORKOUT
 	TYPE states IS (ST0, ST1, ST2, ST3, ST4, ST5, ST6, ST7);
 	SIGNAL PS, NS                              : states;
 	
+	SIGNAL JUMLAH_WORKOUT, OPTIMAL_WORKOUT     : STD_LOGIC (2 downto 0);
+	-- JUMLAH_WORKOUT : Output FSM
+	-- OPTIMAL_WORKOUT : Output FSM (OPTIMAL_WORKOUT(2) terpakai sebagai OPT_Q2)
 	
 	
 	--dijadikan intermediate signal agar bisa dimasukkan
 	--ke dalam sensitivity list. Tidak ada di proteus
 	SIGNAL ALL_0_AND_TOGGLE                    : STD_LOGIC;
-	SIGNAL JUMLAH_WORKOUT                      : STD_LOGIC (2 downto 0);
+	
 BEGIN
 	--dijadikan intermediate signal agar bisa dimasukkan
 	--ke dalam sensitivity list
@@ -62,8 +80,6 @@ BEGIN
 
 	comb_proc : PROCESS (PS, I) IS
 	BEGIN
-		
-
 		CASE PS IS
 			WHEN ST0 =>
 				IF (N = '1') THEN
