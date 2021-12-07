9b4af8a870e84e07d1eaf73a94c0655bd928a157
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-07 10:42:28 +0700

    Beautified

--- NicePoorLogic.vhd
+++ NicePoorLogic.vhd
@@ -1,19 +1,26 @@
---VHDL code for 
+--sebelum push, format dulu di
+--https://g2384.github.io/VHDLFormatter/
+--pilih UPPERCASE semua
+--Show More Settings
+--NEW LINE semua
+--centang Align Signs in all places Mode global, align comments juga
+--centang customise Indentation, \t (one tab aja tulisannya)
+--centang add a new line at the end of file
 
---library and pacakge
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY nicePoorLogic IS
-	PORT (
-		IS_7, OPT_Q2, OPT_Q2' : IN STD_LOGIC;
-		1A_, 1B_, 1C_, 1D_, 1E_, 1F_, 1G_ : IN STD_LOGIC;
+	PORT
+	(
+		IS_7, OPT_Q2, OPT_Q2'             : IN  STD_LOGIC;
+		1A_, 1B_, 1C_, 1D_, 1E_, 1F_, 1G_ : IN  STD_LOGIC;
 		2A_, 2B_, 2C_, 2D_, 2E_, 2F_, 2G_ : OUT STD_LOGIC;
 
-		1A, 1B, 1C, 1D, 1E, 1F, 1G : OUT STD_LOGIC;
-		2A, 2B, 2C, 2D, 2E, 2F, 2G : OUT STD_LOGIC;
-		3A, 3B, 3C, 3D, 3E, 3F, 3G : OUT STD_LOGIC;
-		4A, 4B, 4C, 4D, 4E, 4F, 4G : OUT STD_LOGIC
+		1A, 1B, 1C, 1D, 1E, 1F, 1G        : OUT STD_LOGIC;
+		2A, 2B, 2C, 2D, 2E, 2F, 2G        : OUT STD_LOGIC;
+		3A, 3B, 3C, 3D, 3E, 3F, 3G        : OUT STD_LOGIC;
+		4A, 4B, 4C, 4D, 4E, 4F, 4G        : OUT STD_LOGIC
 	);
 END nicePoorLogic;
 
